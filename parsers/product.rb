9dc5fb66876b8ed5c1d8d6afa060ac4494a54456
kgenas <kKangenas@gmail.com>
2020-11-01 16:48:52 -0600

    modified product and search

--- parsers/product.rb
+++ parsers/product.rb
@@ -1,4 +1,7 @@
 html = Nokogiri.HTML(content)
+#doc = Nokogiri::HTML(URI.open('https://nokogiri.org/tutorials/installing_nokogiri.html'))
+# require 'open-uri'
+#doc = Nokogiri::HTML(URI.open("http://www.threescompany.com/"))
 
 status = html.css('#vi-itm-cond').text
 outputs << {
--- parsers/search.rb
+++ parsers/search.rb
@@ -2,9 +2,10 @@ html = Nokogiri.HTML(content)
 
 products = html.css('.srp-results .s-item')
 products.each do |product|
-  name = product.css('.s-item__title').text.strip
+  name = product.css('.s-item__title').text.strip # strip delete spice
   price = product.css('.s-item__price').text.strip
-  url = product.css('.s-item__link').attr('href')
+  url = product.css('.s-item__link').attr('href') # you are retrieving directly the value of href attribute just like 
+  # any other attribute, so it will return the exact value rendered in the HTML.
   page = {
     url: url,
     page_type: "product",
@@ -15,3 +16,4 @@ products.each do |product|
   }
   pages << page
 end
+ 
